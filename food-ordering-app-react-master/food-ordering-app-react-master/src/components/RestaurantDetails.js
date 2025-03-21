191f202f5f1980bce2420ac29c1731d3f8cf5af6
VaibhavKharvi <vaibhavkharvi08@gmail.com>
2025-03-21 15:10:32 +0530

    Added food

---
+++ food-ordering-app-react-master/food-ordering-app-react-master/src/components/RestaurantDetails.js
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom";
+import { ShimmerBlock, ShimmerLines } from "./Shimmer";
+import useRestaurantDetails from "../utils/useRestaurantDetails";
+import RestaurantCategory from "./RestaurantCategory";
+import RestaurantInfo from "./RestaurantInfo";
+import CartNavigation from "./CartNavigation";
+
+const RestaurantDetails = () => {
+  const { resId } = useParams();
+
+  const restaurant = useRestaurantDetails(resId);
+
+  console.log("restaurant details", restaurant);
+
+  return !restaurant ? (
+    <ShimmerLines />
+  ) : (
+    <div className="restaurant-details">
+      <RestaurantInfo restaurant={restaurant?.info} />
+      <RestaurantCategory menu={restaurant?.menu} />
+    </div>
+  );
+};
+
+export default RestaurantDetails;
